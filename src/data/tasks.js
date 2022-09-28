a0c4977c30a8e8d627ae491561922bb53f04dcf7
190091 <eduardo.martinez@capgemini.com>
2022-09-28 14:55:12 -0500

    First commit

---
+++ src/data/tasks.js
@@ -0,0 +1,22 @@
+export const tasks = [
+  {
+    id: 0,
+    title: "Mi primera tarea",
+    description: "Descripcion de mi primera tarea",
+  },
+  {
+    id: 1,
+    title: "Mi segunda tarea",
+    description: "Descripcion de mi segunda tarea",
+  },
+  {
+    id: 2,
+    title: "Mi tercer tarea",
+    description: "Descripcion de mi tercer tarea",
+  },
+  {
+    id: 3,
+    title: "Mi cuarta tarea",
+    description: "Descripcion de mi cuarta tarea",
+  },
+];
